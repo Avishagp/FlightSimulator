758a3e27c23d357a03783c8580a44482375ec834
Avishagp <44115709+Avishagp@users.noreply.github.com>
2019-04-24 19:38:38 +0300

    and NOW the settings window has normal textboxes, which I find amazingly wowing

--- Views/Windows/SettingsWindow.xaml.cs
+++ Views/Windows/SettingsWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace FlightSimulator.Views.Windows
         public SettingsWindow()
         {
             InitializeComponent();
+            //DataContext = new SettingsWindowViewModel(WHAT MODEL?)
         }
 
     }

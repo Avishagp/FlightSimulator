f65422618f46f5149eedd545bb708771558fc97d
Avishagp <44115709+Avishagp@users.noreply.github.com>
2019-04-27 14:55:44 +0300

    Joystick now sends messages to server! KABOOM

--- ViewModels/ControlPanelViewModel.cs
+++ ViewModels/ControlPanelViewModel.cs
@@ -19,19 +19,23 @@ namespace FlightSimulator.ViewModels
             this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
         }
 
-        public void SendMessagesToSim(object parameter)
+        public void SendMessagesToSim(object parameter, bool fromTextBox = false)
         {
             autoTextBoxChanged = true;
-            Color = "White";
+            if (fromTextBox)
+            {
+                Color = "White";
+            }
             Thread send_to_sim = new Thread(new ParameterizedThreadStart(DataWriterClient.Instance.SendMassages));
             send_to_sim.Start(parameter);
         }
+
         public ICommand _autoPilotOKCommand;
         public ICommand autoPilotOKCommand
         {
             get
             {
-                return _autoPilotOKCommand ?? (_autoPilotOKCommand = new CommandHandler(() => SendMessagesToSim(textBox)));
+                return _autoPilotOKCommand ?? (_autoPilotOKCommand = new CommandHandler(() => SendMessagesToSim(textBox, true)));
             }
         }
         
@@ -81,6 +85,66 @@ namespace FlightSimulator.ViewModels
             {
                 color = value;
                 NotifyPropertyChanged("Color");
+            }
+        }
+
+        public double throttle;
+        public double ThrottleVal
+        {
+            set
+            {
+                throttle = value;
+                string msg = "set " + "/controls/engines/current-engine/throttle " + throttle;
+                SendMessagesToSim(msg);
+            }
+            get
+            {
+                return throttle;
+            }
+        }
+
+        public double rudder;
+        public double RudderVal
+        {
+            set
+            {
+                rudder = value;
+                string msg = "set " + "/controls/flight/rudder " + rudder;
+                SendMessagesToSim(msg);
+            }
+            get
+            {
+                return rudder;
+            }
+        }
+
+        public double aileron;
+        public double AileronVal
+        {
+            set
+            {
+                aileron = value;
+                string msg = "set " + "/controls/flight/aileron " + aileron;
+                SendMessagesToSim(msg);
+            }
+            get
+            {
+                return aileron;
+            }
+        }
+
+        public double elevator;
+        public double ElevatorVal
+        {
+            set
+            {
+                elevator = value;
+                string msg = "set " + "/controls/flight/elevator " + elevator;
+                SendMessagesToSim(msg);
+            }
+            get
+            {
+                return elevator;
             }
         }
     }
